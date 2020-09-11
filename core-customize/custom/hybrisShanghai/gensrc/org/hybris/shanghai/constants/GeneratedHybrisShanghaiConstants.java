ee7e819cbdfd6a55b0f690bc112cbfaa48c9bd47
tonyjiangde <tao.jiang02@sap.com>
2020-09-11 10:53:09 +0800

    hybrisShanghai
    hybrisShanghai

---
+++ core-customize/custom/hybrisShanghai/gensrc/org/hybris/shanghai/constants/GeneratedHybrisShanghaiConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Jul 29, 2019 2:14:29 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package org.hybris.shanghai.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedHybrisShanghaiConstants
+{
+	public static final String EXTENSIONNAME = "hybrisShanghai";
+	
+	protected GeneratedHybrisShanghaiConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ core-customize/custom/hybrisShanghai/gensrc/org/hybris/shanghai/jalo/GeneratedHybrisShanghaiManager.java
@@ -0,0 +1,46 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Jul 29, 2019 2:14:29 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package org.hybris.shanghai.jalo;
+
+import de.hybris.platform.jalo.Item;
+import de.hybris.platform.jalo.Item.AttributeMode;
+import de.hybris.platform.jalo.extension.Extension;
+import java.util.HashMap;
+import java.util.Map;
+import org.hybris.shanghai.constants.HybrisShanghaiConstants;
+
+/**
+ * Generated class for type <code>HybrisShanghaiManager</code>.
+ */
+@SuppressWarnings({"deprecation","unused","cast","PMD"})
+public abstract class GeneratedHybrisShanghaiManager extends Extension
+{
+	protected static final Map<String, Map<String, AttributeMode>> DEFAULT_INITIAL_ATTRIBUTES;
+	static
+	{
+		final Map<String, Map<String, AttributeMode>> ttmp = new HashMap();
+		DEFAULT_INITIAL_ATTRIBUTES = ttmp;
+	}
+	@Override
+	public Map<String, AttributeMode> getDefaultAttributeModes(final Class<? extends Item> itemClass)
+	{
+		Map<String, AttributeMode> ret = new HashMap<>();
+		final Map<String, AttributeMode> attr = DEFAULT_INITIAL_ATTRIBUTES.get(itemClass.getName());
+		if (attr != null)
+		{
+			ret.putAll(attr);
+		}
+		return ret;
+	}
+	
+	@Override
+	public String getName()
+	{
+		return HybrisShanghaiConstants.EXTENSIONNAME;
+	}
+	
+}
